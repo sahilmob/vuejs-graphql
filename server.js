7fe54d88dcc0558280b886cf555bae85698d1f04
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-03 21:26:33 +0300

    Add db connection configs.

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const { ApolloServer, gql } = require('apollo-server');
+const mongoose = require('mongoose')
+require('dotenv').config({ path: './variables.env' })
 
-
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => {
+    console.log('DB connected');
+}).catch(e => {
+    console.log(e);
+})
 
 const typeDefs = gql`
 type Todo {
