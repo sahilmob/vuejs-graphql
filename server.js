2756af34ad6bd967ed667a5f68c0170df69cf196
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-03 22:26:19 +0300

    create resolvers file and add signupUser mutation

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const path = require('path')
 
 const filePath = path.join(__dirname, 'typeDefs.gql')
 const typeDefs = fs.readFileSync(filePath, 'utf8')
+const resolvers = require('./resolvers.js')
 
 require('dotenv').config({ path: './variables.env' })
 const User = require('./models/User.js')
@@ -19,6 +20,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => {
 
 const server = new ApolloServer({
     typeDefs,
+    resolvers,
     context: {
         User,
         Post
