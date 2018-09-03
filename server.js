2b9796af488cda088a7a18b24d327b4ba8ffafdf
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-03 22:05:06 +0300

    Add User and Post models

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const { ApolloServer, gql } = require('apollo-server');
 const mongoose = require('mongoose')
+
 require('dotenv').config({ path: './variables.env' })
+const User = require('./models/User.js')
+const Post = require('./models/Post.js')
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => {
     console.log('DB connected');
@@ -20,7 +23,11 @@ type Query {
 `
 
 const server = new ApolloServer({
-    typeDefs
+    typeDefs,
+    context: {
+        User,
+        Post
+    }
 });
 
 server.listen().then(({ url }) => {
