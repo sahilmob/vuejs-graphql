54acf87770f1da26f8ab4f162ecac4e1d4e2b0a5
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-04 22:04:29 +0300

    Add carousel to the home page

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,13 +1,19 @@
 <template>
-  <v-container>
-    <h1>Home</h1>
+  <v-container text-xs-center v-if="getPosts">
+    <v-flex xs12>
+      <v-carousel v-bind="{'cycle': true }" interval="3000">
+        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+          <h1 id="carousel__title">{{post.title}}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
     <!-- <div v-if="$apollo.loading">Loading...</div>
     <ul e-else v-for="post in getPosts" :key="post._id">
       <li>
         {{post.title}} {{post.imageUrl}} {{post.description}}
       </li>
     </ul> -->
-    <ApolloQuery :query="getPostsQuery">
+    <!-- <ApolloQuery :query="getPostsQuery">
       <template slot-scope="{result: {loading, error, data}}">
         <div v-if="loading">Loading...</div>
         <div v-else-if="error">{{error.message}}</div>
@@ -15,7 +21,7 @@
           {{post.title}} {{post.imageUrl}} {{post.likes}}
         </ul>
       </template>
-    </ApolloQuery>
+    </ApolloQuery> -->
   </v-container>
 </template>
 
@@ -26,38 +32,53 @@ export default {
   name: "home",
   data() {
     return {
-      getPostsQuery: gql`
+      // getPostsQuery: gql`
+      //   query {
+      //     getPosts {
+      //       _id
+      //       title
+      //       description
+      //       imageUrl
+      //       likes
+      //     }
+      //   }
+      // `
+    };
+  },
+  apollo: {
+    getPosts: {
+      query: gql`
         query {
           getPosts {
             _id
             title
             description
             imageUrl
-            likes
           }
         }
-      `
-    };
+      `,
+      result(args) {
+        console.log(args);
+      },
+      error(err) {
+        console.log("Error", err);
+      }
+    }
   }
-  // apollo: {
-  //   getPosts: {
-  //     query: gql`
-  //       query {
-  //         getPosts {
-  //           _id
-  //           title
-  //           description
-  //           imageUrl
-  //         }
-  //       }
-  //     `
-  // result(args) {
-  //   console.log(args);
-  // },
-  // error(err) {
-  //   console.log("Error", err);
-  // }
-  // }
-  // }
 };
 </script>
+
+<style scoped>
+#carousel__title {
+  position: absolute;
+  background-color: rgba(0, 0, 0, 0.5);
+  color: white;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
+
