111d0c17c7020790f20154a284cb104ac3916483
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-04 21:22:11 +0300

    Examples about smart quries

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <v-container>
     <h1>Home</h1>
-    <ul v-for="post in getPosts" :key="post._id">
+    <div v-if="$apollo.loading">Loading...</div>
+    <ul e-else v-for="post in getPosts" :key="post._id">
       <li>
         {{post.title}} {{post.imageUrl}} {{post.description}}
       </li>
@@ -14,6 +15,11 @@
 import { gql } from "apollo-boost";
 export default {
   name: "home",
+  data() {
+    return {
+      posts: []
+    };
+  },
   apollo: {
     getPosts: {
       query: gql`
@@ -26,6 +32,12 @@ export default {
           }
         }
       `
+      // result(args) {
+      //   console.log(args);
+      // },
+      // error(err) {
+      //   console.log("Error", err);
+      // }
     }
   }
 };
