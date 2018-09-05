3fa354033629f5e72d1a6fae7cfc520e1a220cae
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-05 17:04:28 +0300

    Display loading spinner while data is loading, and use mapGetters

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,7 +1,16 @@
 <template>
   <v-container text-xs-center>
+    <v-layout row>
+      <v-dialog v-model="loading" persistent fullscreen>
+        <v-container fill-height>
+          <v-layout row justify-center align-center>
+            <v-progress-circular indeterminate :size="70" :width="7" color="secondary"></v-progress-circular>
+          </v-layout>
+        </v-container>
+      </v-dialog>
+    </v-layout>
     <v-flex xs12>
-      <v-carousel v-if="posts.length > 0" v-bind="{'cycle': true }" interval="3000">
+      <v-carousel v-if="!loading && posts.length > 0" v-bind="{'cycle': true }" interval="3000">
         <v-carousel-item v-for="post in posts" :key="post._id" :src="post.imageUrl">
           <h1 id="carousel__title">{{post.title}}</h1>
         </v-carousel-item>
@@ -28,6 +37,8 @@
 <script>
 // @ is an alias to /src
 import { gql } from "apollo-boost";
+import { mapGetters } from "vuex";
+
 export default {
   created() {
     this.handleGetCarouselPosts();
@@ -49,9 +60,7 @@ export default {
     };
   },
   computed: {
-    posts() {
-      return this.$store.getters.posts;
-    }
+    ...mapGetters(["loading", "posts"])
   },
   methods: {
     handleGetCarouselPosts() {
--- client/src/store.js
+++ client/src/store.js
@@ -3,21 +3,25 @@ import Vuex from 'vuex'
 
 import { gql } from 'apollo-boost'
 import { defaultClient as apolloClient } from './main'
-import { stat } from 'fs';
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    posts: []
+    posts: [],
+    loading: false
   },
   mutations: {
     setPosts: (state, posts) => {
       state.posts = posts
+    },
+    setLoading: (state, payload) => {
+      state.loading = payload;
     }
   },
   actions: {
     getPosts: ({ commit }) => {
+      commit('setLoading', true)
       apolloClient
         .query({
           query: gql`
@@ -30,10 +34,12 @@ export default new Vuex.Store({
           }
         `
         }).then(({ data }) => {
-          commit('setPosts', data.getPosts)
+          commit('setPosts', data.getPosts);
+          commit('setLoading', false);
           console.log(data.getPosts);
         })
         .catch(err => {
+          commit('setLoading', false);
           console.log(err);
         })
     }
@@ -41,6 +47,9 @@ export default new Vuex.Store({
   getters: {
     posts: (state) => {
       return state.posts
+    },
+    loading: (state) => {
+      return state.loading
     }
   }
 })
