e53f58a94e6341abe27b82ed4b47c7f483493e45
Sahil Mobaidin <sahil.hmob@hotmail.com>
2018-09-05 21:19:27 +0300

    Pass current user to the context object and verify user using the token

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, AuthenticationError } = require('apollo-server');
 const mongoose = require('mongoose')
 const fs = require('fs')
 const path = require('path')
+const jwt = require('jsonwebtoken')
 
 const filePath = path.join(__dirname, 'typeDefs.gql')
 const typeDefs = fs.readFileSync(filePath, 'utf8')
@@ -17,13 +18,23 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }).then(() => {
     console.log(e);
 })
 
+const getUser = async (token) => {
+    if (token) {
+        try {
+            let user = await jwt.verify(token, process.env.SECRET)
+            console.log(user);
+        } catch (err) {
+            throw new AuthenticationError('Your session has ended. Please sign in again')
+        }
+    }
+}
 
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    context: {
-        User,
-        Post
+    context: async ({ req }) => {
+        const token = req.headers["authorization"]
+        return { User, Post, currentUser: await getUser(token) }
     }
 });
 
