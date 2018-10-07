d214ea91faea05ac9e9ff3a003e3edf3baa6a060
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-10-07 19:48:21 +0300

    create add post form

--- client/src/components/Posts/AddPost.vue
+++ client/src/components/Posts/AddPost.vue
@@ -1,12 +1,95 @@
 <template>
-    <v-container>
-        <h1>Add Post</h1>
+    
+        <v-container text-xs-center mt-5 pt-5>
+        <v-layout row wrap>
+            <v-flex xs12 sm6 offset-sm3>
+                <h1 class="primary--text">Add Post</h1>
+            </v-flex>
+        </v-layout>
+
+        <v-layout row wrap>
+            <v-flex xs12 sm6 offset-sm3>    
+                        <v-form v-model="isFormValid" lazy-validation ref="form" @submit.prevent="handleAddPost">
+                            <!-- Title input -->
+                            <v-layout row>
+                                <v-flex xs12>
+                                    <v-text-field :rules="titleRules" v-model="title" label="Post Title" type="text" required></v-text-field>
+                                </v-flex>
+                            </v-layout>
+                            <!-- Image url -->
+
+                            <v-layout row>
+                                <v-flex xs12>
+                                    <v-text-field :rules="imageRules" v-model="imageUrl" label="Image Url" type="text" required></v-text-field>
+                                </v-flex>
+                            </v-layout>
+                           
+                            <!-- Image Preview -->
+                            <v-layout row>
+                                    <v-flex xs12>
+                                        <img :src="imageUrl" height="300px">
+                                    </v-flex>
+                            </v-layout>
+
+                            <!-- Category Select -->
+                             <v-layout row>
+                                    <v-flex xs12>
+                                        <v-select :rules="categoriesRules" v-model="categories" :items="['Art', 'Education', 'travel', 'Photography', 'Technology']" multiple label="Categories">
+                                        </v-select>
+                                    </v-flex>
+                            </v-layout>
+
+                            <!-- Descripton -->
+                            <v-layout row>
+                                    <v-flex xs12>
+                                        <v-textarea :rules="descRules" v-model="description" label="Description">
+                                        </v-textarea>
+                                    </v-flex>
+                            </v-layout>
+
+
+                            <v-layout row>
+                                <v-flex xs12>
+                                    <v-btn :loading="loading" color="info" type="submit" :disabled="loading || !isFormValid" @click.native="loader = 'loading'">
+                                        Submit
+                                        </v-btn>
+                                </v-flex>
+                            </v-layout>
+                        </v-form>
+            </v-flex>
+        </v-layout>
     </v-container>
 </template>
 
 <script>
 export default {
-  name: "Add Post"
+  name: "Add Post",
+  data(){
+      return{
+          isFormValid: true,
+          title: '',
+          imageUrl: '',
+          categories: [],
+          description: '',
+          titleRules: [
+              title => !! title || "Title is required",
+              title => title.length <= 20 || "Title must have less than 20 characters"
+          ],
+          titleRules: [
+              title => !! title || "Title is required"
+          ],
+          timageRules: [
+              image => !! image || "Image is required"
+          ],
+          categoriesRules: [
+              categories => !! categories.length > 0 || "At least one category is required"
+          ],
+          descRules: [
+              desc => !! desc  || "Description is required",
+              desc => desc.length <= 200 || "Description must have less than 200 characters"
+          ]
+      }
+  }
 };
 </script>
 
