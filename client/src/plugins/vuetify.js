0b57bd623931d714143a00aad7002a46bc7bc902
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-10-31 18:26:11 +0200

    add update user post dialog

--- client/src/plugins/vuetify.js
+++ client/src/plugins/vuetify.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
 
 Vue.use(Vuetify, {
-    theme: {
-        primary: "#42b883",
-        secondary: "#35495e",
-        accent: "#427AA1",
-        error: "#FF495C",
-        warning: "#F7B801",
-        info: "#336699",
-        success: "#72A98F"
-    }
-})
+	theme: {
+		primary: "#42b883",
+		secondary: "#35495e",
+		accent: "#427AA1",
+		error: "#FF495C",
+		warning: "#F7B801",
+		info: "#336699",
+		success: "#72A98F"
+	}
+});
