819b43ff278b63a6f13e30553b625676418425d6
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-01 08:42:29 +0200

    reftech posts after adding new post

--- client/src/store.js
+++ client/src/store.js
@@ -11,7 +11,8 @@ import {
 	SEARCH_POST,
 	GET_USER_POSTS,
 	UPDATE_USER_POST,
-	DELETE_USER_POST
+	DELETE_USER_POST,
+	INFINITE_SCROLL_POSTS
 } from "./queries";
 
 Vue.use(Vuex);
@@ -118,7 +119,17 @@ export default new Vuex.Store({
 							_id: -1,
 							...payload
 						}
-					}
+					},
+					// Rerun specified queries perform the mutation in order to get fresh data
+					refetchQueries: [
+						{
+							query: INFINITE_SCROLL_POSTS,
+							variables: {
+								pageNum: 1,
+								pageSize: 2
+							}
+						}
+					]
 				})
 				.then(({ data }) => {
 					commit("setLoading", false);
