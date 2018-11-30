e8503c35a3585de0afdf1a590dc6b05f0cff8ce9
Sahil H. Mobaidin <Sahil.hmob@hotmail.com>
2018-11-30 22:27:00 +0200

    prepare for deployment

--- client/src/main.js
+++ client/src/main.js
@@ -16,7 +16,7 @@ Vue.component("form-alert", FormAlert);
 Vue.use(VueApollo);
 
 export const defaultClient = new ApolloClient({
-	uri: "http://localhost:4000/graphql",
+	uri: "https://fullstack-vue-graphql-fodjsrudon.now.sh/graphql",
 	fetchOptions: {
 		credentials: "include"
 	},
